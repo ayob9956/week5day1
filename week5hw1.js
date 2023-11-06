938e093b92ae9cacbddea13b406bbe11d277c4ab
ayob9956 <ayobalayed@gmail.com>
2023-11-06 14:20:30 +0300

    hw

---
+++ week5hw1.js
@@ -0,0 +1,20 @@
+var inp = document.getElementById("inp")
+
+
+function btn1() {
+    window.open(inp.value, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=375,height=812");
+}   
+
+
+function btn2() {
+    window.open(inp.value, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=412,height=869");
+} 
+
+function btn3() {
+    window.open(inp.value, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=1024,height=1366");
+} 
+
+function btn4() {
+    window.open(inp.value, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=1920,height=1080");
+     
+} 
